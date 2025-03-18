d645582903ee1738e96aef46abb7cfac7579d1a1
msaadidi <saadidimouataz@gmail.com>
2025-03-18 01:59:05 +0000

    redis is setup

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -9,6 +9,7 @@ mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --allow-root
 
+
 mv wp-config-sample.php wp-config.php
 
 sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', '${MYSQL_DATABASE}' );/" wp-config.php
@@ -31,11 +32,19 @@ wp core install \
   --admin_email="${WP_ADMIN_EMAIL}" \
   --allow-root
 
+
 wp user create \
     "${WP_USER}" "${WP_USER_EMAIL}" \
     --user_pass="${WP_USER_PASSWORD}" \
     --role=editor \
     --allow-root
+
+wp plugin install redis-cache --activate --allow-root
+
+wp config set WP_REDIS_HOST "'${WP_REDIS_HOST}'" --add --allow-root
+wp config set WP_REDIS_PASSWORD "'${WP_REDIS_PASSWORD}'" --add --allow-root
+
+wp redis enable --allow-root
 
 mkdir -p /run/php
 
