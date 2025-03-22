4db5921a10a687e63ea53c76c736026ee74077d5
Mouataz Saadidi <saadidimouataz@gmail.com>
2025-03-22 06:54:35 +0000

    everything is working + time to refactor

---
+++ srcs/requirements/bonus/ftp/tools/ftp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p /var/www/wordpress /var/run/vsftpd/empty
+useradd -d /var/www/wordpress ${FTP_USER} -s /bin/bash && \
+    echo "${FTP_USER}:${FTP_PASS}" | chpasswd
+
+chown -R ${FTP_USER}:www-data /var/www/wordpress
+chmod -R 775 /var/www/wordpress
+
+exec /usr/sbin/vsftpd /etc/vsftpd.conf
